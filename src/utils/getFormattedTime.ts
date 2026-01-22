a5080e1e75527c61cc5c606f9cd319c8033689a8
Rostyslav <rostislavkpi@gmail.com>
2026-01-22 10:14:09 +0200

    format time output to always show two digits for hours and minutes

--- src/utils/getFormattedTime.ts
+++ src/utils/getFormattedTime.ts
@@ -1,8 +1,8 @@
 export const getFormattedTime = () => {
   const now = new Date();
 
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
 
   return `${hours}:${minutes}`;
 };
